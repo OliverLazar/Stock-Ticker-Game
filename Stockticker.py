20c42b5d0779c7e8b45e989f66428a015725cfd9
oliver <oliver.lazar01@gmail.com>
2024-11-20 00:30:40 -0500

    Signed-off-by: Oliver Lazar <oliver.lazar01@gmail.com>
    test

--- Stockticker.py
+++ Stockticker.py
@@ -11,6 +11,7 @@ y=350
 x = 110
 screen = pygame.display.set_mode((1000, 700))
 Clock = pygame.time.Clock()
+pygame.display.set_caption("Stock Ticker Game")
 col_font = pygame.font.SysFont("System", 25)
 
 FPS = 60
@@ -19,6 +20,9 @@ rectangles = {}
 
 width = int(1000/9)
 height = int(700/11 * 0.4)
+
+highlight_row = 1
+highlight_col = 1
 
 def grid():
     for r in range(n_row):
@@ -36,6 +40,11 @@ def change_cell_text(row, col, text, color):
         text_rect = text_surface.get_rect(center=rect.center)
         screen.blit(text_surface, text_rect)
 
+def draw_highlight(row, col, color):
+    if (row, col) in rectangles:
+        rect = rectangles[(row, col)]
+        pygame.draw.rect(screen, color, rect, 4)
+
 run = True
 
 while run:
@@ -45,15 +54,26 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and highlight_row > 1 :
+                highlight_row -= 1
+            if event.key == pygame.K_DOWN and highlight_row < n_row - 1:
+                highlight_row += 1
+            if event.key == pygame.K_LEFT and highlight_col > 0 and highlight_col > 1:
+                highlight_col -= 1
+            if event.key == pygame.K_RIGHT and highlight_col < 6:
+                highlight_col += 1
+
 
 
     grid()
+
     for row in range(1,11):
         change_cell_text(row,0, players[row-1] ,"green")
     for col in range(0,9):
         change_cell_text(0, col, columns[col], color_cycle[col])
 
-
+    draw_highlight(highlight_row, highlight_col, color_cycle[highlight_col])
 
 
     Clock.tick(FPS)
